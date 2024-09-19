ade46f81f240847bc8d2635f65f6a5430286ef58
alexsalas <alexsalas@ceroca.cat>
2024-09-19 19:31:04 +0200

    19/09
    ERROR

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
+    private Vector2 minPantalla;
+    private Vector2 maxPantalla;
 
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
     }
+
     void Update()
     {
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
 
-      //debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
-       Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
-       Vector2 novaPos = transform.position;
-        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
-        transform.position = novaPos;
+        Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
 
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
+        Vector2 novaPos = transform.position;
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x - 0.75f;
+        minPantalla.y = minPantalla.y + 0.75f;
+
+        transform.position = novaPos;
     }
 }
